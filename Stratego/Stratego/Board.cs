391edf62564020c45ab254818d95334e76782aa5
dawid_szewczyk <dawid.szewczyk19@gmail.com>
2018-05-10 15:08:25 +0200

    Klasa Board

---
+++ Stratego/Stratego/Board.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratego
+{
+    class Board
+    {
+        int[,] board;
+
+        public Board(int n) {
+            board = new int[n,n];
+        }
+
+        public int GetPointsForMove(int lastChoiceColumn, int lastChoiceRow) {
+            
+        }
+
+        public bool[,] GetAvaliableMoves() {
+            bool[,] result = new bool[board.GetLength(0), board.GetLength(1)];
+
+            for (int i = 0; i < board.GetLength(0); i++) {
+                for (int j = 0; j < board.GetLength(1); j++) {
+                    result[i, j] = board[i, j] == 0;
+                }
+            }
+
+            return result;
+        }
+    }
+}
