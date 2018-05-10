2b5a9c6afe63dd8a1981c5b5a23aff4d69367a85
MrG <gieem17@gmail.com>
2018-05-10 15:26:19 +0200

    klasy i interfejsy

---
+++ Stratego/Stratego/Algorithm/AlphaBetaPruning.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratego
+{
+    class AlphaBetaPruning : IAlgorithm
+    {
+        public Tuple<int, int> ChooseNextMove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Stratego/Stratego/Algorithm/Minmax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratego
+{
+    class Minmax : IAlgorithm
+    {
+        public Tuple<int, int> ChooseNextMove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
